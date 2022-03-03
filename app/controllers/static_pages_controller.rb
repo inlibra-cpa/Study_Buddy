a1e071d954fc9b6bbdc4f587a4d34ffd152d72dc
inlibra-cpa <inlibra@gmail.com>
2022-03-02 21:22:20 -0500

    styling courses and home_page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:home_page]
   def home_page
-  end
-
-  def privacy_policy
+    @courses = Course.all.limit(3)
+    @latest_couses = Course.all.limit(3).order(created_at: :desc)
   end
 end
