6d48f1cc2428869eb57bc9aaac832c9bc1d38fcd
inlibra-cpa <inlibra@gmail.com>
2022-03-03 19:58:35 -0500

    devise trackable and confirmable

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
+  resources :users, only: [:index]
   root "static_pages#home_page"
-  get "privacy_policy", to: "static_pages#privacy_policy"
+  get "static_pages/home_page"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
