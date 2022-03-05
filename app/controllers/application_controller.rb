764f9f0716ee361c8a5abce7eeb6353bcb83a028
inlibra-cpa <inlibra@gmail.com>
2022-03-05 14:55:07 -0500

    friendly_id for users and if online? feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,10 @@ class ApplicationController < ActionController::Base
 
     private
 
+    def user_activity
+      current_user.try :touch
+    end
+
     def user_not_authorized #pundit
       flash[:alert] = "You are not authorized to perform this action."
       redirect_to(request.referrer || root_path)
