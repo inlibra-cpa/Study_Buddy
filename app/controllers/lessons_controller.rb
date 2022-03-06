8b76312c7e697937a51ded883687e5f9279be94d
inlibra-cpa <inlibra@gmail.com>
2022-03-05 19:49:42 -0500

    nested resourses for lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,33 +1,30 @@
 class LessonsController < ApplicationController
   before_action :set_lesson, only: %i[ show edit update destroy ]
 
-  # GET /lessons or /lessons.json
   def index
     @lessons = Lesson.all
   end
 
-  # GET /lessons/1 or /lessons/1.json
   def show
     authorize @lesson
   end
 
-  # GET /lessons/new
   def new
     @lesson = Lesson.new
+    @course = Course.friendly.find(params[:course_id])
   end
 
-  # GET /lessons/1/edit
   def edit
     authorize @lesson
   end
 
-  # POST /lessons or /lessons.json
   def create
     @lesson = Lesson.new(lesson_params)
-
+    @course = Course.friendly.find(params[:course_id])
+    @lesson.course_id = @course.id
     respond_to do |format|
       if @lesson.save
-        format.html { redirect_to lesson_url(@lesson), notice: "Lesson was successfully created." }
+        format.html { redirect_to course_lesson_path(@course, @lesson), notice: 'Lesson was successfully created.' }
         format.json { render :show, status: :created, location: @lesson }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -36,12 +33,11 @@ class LessonsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /lessons/1 or /lessons/1.json
   def update
     authorize @lesson
     respond_to do |format|
       if @lesson.update(lesson_params)
-        format.html { redirect_to lesson_url(@lesson), notice: "Lesson was successfully updated." }
+        format.html { redirect_to course_lesson_path(@course, @lesson), notice: 'Lesson was successfully updated.' }
         format.json { render :show, status: :ok, location: @lesson }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -50,7 +46,6 @@ class LessonsController < ApplicationController
     end
   end
 
-  # DELETE /lessons/1 or /lessons/1.json
   def destroy
     authorize @lesson
     @lesson.destroy
@@ -62,8 +57,8 @@ class LessonsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_lesson
+      @course = Course.friendly.find(params[:course_id])
       @lesson = Lesson.friendly.find(params[:id])
     end
 
