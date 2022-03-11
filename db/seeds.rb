dfb99a35cfc995b2b2dd98d86128a9b91c247e9b
inlibra-cpa <inlibra@gmail.com>
2022-03-11 12:28:21 -0500

    last changes in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #)
 #user.skip_confirmation!
 #user.save!
-
+PublicActivity.enabled = false
 30.times do
     Course.create!([{
       title: Faker::Educator.course_name,
@@ -18,3 +18,4 @@
       price: Faker::Number.between(from: 100, to: 20000)
     }])
 end 
+PublicActivity.enabled = true
